b27aea29b52c308d97b2cdef823503aa9a03aa2f
mzxmzx520 <jerry_judy@hotmail.com>
2015-09-29 19:23:20 -0700

    ADDED NEW COMMENT AGAIN

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -19,7 +19,7 @@ import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
- * NEWCOMMENT Entry point classes define <code>onModuleLoad()</code>.
+ * NEWCOMMENT123 Entry point classes define <code>onModuleLoad()</code>.
  */
 public class GitLab implements EntryPoint {
 	/**
